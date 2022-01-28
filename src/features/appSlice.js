ffc07cc04b0623caceaa235a4de4c1ebf0919ad3
Jumba Mark <jumbamark@yahoo.com>
2022-01-28 15:57:15 +0300

    modified the appSlice

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -6,7 +6,7 @@ const initialState = {
 };
 
 export const appSlice = createSlice({
-  name: "counter",
+  name: "app",
   initialState,
 
   // The `reducers` field lets us define reducers and generate associated actions
@@ -29,6 +29,6 @@ export const {increment, decrement, incrementByAmount} = appSlice.actions;
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
+export const selectCount = (state) => state.app.value;
 
 export default appSlice.reducer;
