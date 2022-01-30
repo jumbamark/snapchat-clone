b059eb6e62d6dca11aaf19bc5b93ef0eab461bb7
Jumba Mark <jumbamark@yahoo.com>
2022-01-30 15:51:19 +0300

    added a dispatcher and redirect functionality

--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -2,6 +2,9 @@ import React, {useCallback, useRef} from "react";
 import "./WebcamCapture.css";
 import Webcam from "react-webcam";
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import {useDispatch} from "react-redux";
+import {setCameraImage} from "./features/cameraSlice";
+import {useNavigate} from "react-router-dom";
 
 const videoConstraints = {
   width: 250,
@@ -12,12 +15,16 @@ const videoConstraints = {
 function WebcamCapture() {
   const webcamRef = useRef(null);
   // const [image, setImage] = useState(null);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     // console.log(imageSrc);
     // setImage(imageSrc);
-  }, [webcamRef]);
+    dispatch(setCameraImage(imageSrc));
+    navigate("/preview"); //redirecting the user to the appropriate page on the webcamCapture
+  }, [webcamRef, dispatch, navigate]);
 
   return (
     <div className="webcamCapture">
@@ -42,3 +49,8 @@ function WebcamCapture() {
 }
 
 export default WebcamCapture;
+
+// redirect
+// When we take a picture it should redirect to the preview route
+// useHistory gives the history of the web pages, we can push a page on(essentially a redirect)
+// If you call the capture button it takes you to "/preview"
