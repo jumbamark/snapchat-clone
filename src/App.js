0af94ae9c8b5c6d7948d25287d355d1414b1531f
Jumba Mark <jumbamark@yahoo.com>
2022-01-30 15:59:39 +0300

    Added the preview route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
 import WebcamCapture from "./WebcamCapture";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Preview from "./Preview";
 
 function App() {
   return (
-    <div className="App">
-      <WebcamCapture />
+    <div className="app">
+      <BrowserRouter>
+        <div className="app-body">
+          <Routes>
+            <Route path="/preview" element={<Preview />} />
+            <Route exact path="/" element={<WebcamCapture />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
