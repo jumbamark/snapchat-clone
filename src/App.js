ce2373500d704a91cbd5fcb91db23840fffbf873
Jumba Mark <jumbamark@yahoo.com>
2022-01-31 19:31:39 +0300

    added the chats router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import WebcamCapture from "./WebcamCapture";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Preview from "./Preview";
+import Chats from "./Chats";
+import ChatView from "./ChatView";
 
 function App() {
   return (
@@ -10,8 +12,10 @@ function App() {
       <BrowserRouter>
         <div className="app-body">
           <Routes>
-            <Route path="/preview" element={<Preview />} />
             <Route exact path="/" element={<WebcamCapture />} />
+            <Route path="/preview" element={<Preview />} />
+            <Route path="/chats" element={<Chats />} />
+            {/* <Route path="/chats/view" element={<ChatView />} /> */}
           </Routes>
         </div>
       </BrowserRouter>
