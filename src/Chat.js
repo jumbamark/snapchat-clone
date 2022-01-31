fc3e37b58b6b661ec0e1c6b9bc45d31188c8395b
Jumba Mark <jumbamark@yahoo.com>
2022-01-31 19:30:30 +0300

    component that maps chats from the database

---
+++ src/Chat.js
@@ -0,0 +1,48 @@
+import {StopRounded} from "@mui/icons-material";
+import {Avatar} from "@mui/material";
+import React from "react";
+import {useDispatch} from "react-redux";
+import ReactTimeago from "react-timeago";
+import "./Chat.css";
+import {selectImage} from "./features/appSlice";
+import {collection, setDoc, doc} from "firebase/firestore";
+import {db} from "./firebase";
+import {useNavigate} from "react-router-dom";
+
+function Chat({id, username, timestamp, read, imageUrl, profilePic}) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const open = () => {
+    if (!read) {
+      // pushes the image into the store
+      dispatch(selectImage(imageUrl));
+      const postsRef = collection(db, "posts");
+      setDoc(
+        doc(postsRef, "id"),
+        {
+          read: true,
+        },
+        {merge: true}
+      );
+
+      navigate("/chats/view");
+    }
+  };
+
+  return (
+    <div className="chat" onClick={open}>
+      <Avatar src={profilePic} className="chat-avatar" />
+      <div className="chat-info">
+        <h4>{username}</h4>
+        <p>
+          Tap to View - <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()} />
+        </p>
+      </div>
+
+      {!read && <StopRounded className="chat-readIcon" />}
+    </div>
+  );
+}
+
+export default Chat;
