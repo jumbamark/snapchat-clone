274126d5d4f750b7c130606fed94dea492f3a458
Jumba Mark <jumbamark@yahoo.com>
2022-02-02 00:51:02 +0300

    Rendered Login, persisted the login, added logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import WebcamCapture from "./WebcamCapture";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Preview from "./Preview";
 import Chats from "./Chats";
 import ChatView from "./ChatView";
+import {login, logout, selectUser} from "./features/appSlice";
+import {useDispatch, useSelector} from "react-redux";
+import Login from "./Login";
+import {onAuthStateChanged} from "firebase/auth";
+import {auth} from "./firebase";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          login({
+            username: user.displayName,
+            profilePicture: user.photoURL,
+            id: user.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <div className="app-body">
-          <Routes>
-            <Route exact path="/" element={<WebcamCapture />} />
-            <Route path="/preview" element={<Preview />} />
-            <Route path="/chats" element={<Chats />} />
-            <Route path="/chats/view" element={<ChatView />} />
-          </Routes>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <img
+              src="https://scx2.b-cdn.net/gfx/news/2017/1-snapchat.jpg"
+              alt=""
+              className="app-logo"
+            />
+            <div className="app-body">
+              <div className="app-bodyBackground">
+                <Routes>
+                  <Route exact path="/" element={<WebcamCapture />} />
+                  <Route path="/preview" element={<Preview />} />
+                  <Route path="/chats" element={<Chats />} />
+                  <Route path="/chats/view" element={<ChatView />} />
+                </Routes>
+              </div>
+            </div>
+          </>
+        )}
       </BrowserRouter>
     </div>
   );
